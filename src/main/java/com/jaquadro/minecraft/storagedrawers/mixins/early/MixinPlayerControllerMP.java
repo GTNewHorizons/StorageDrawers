82ba5efed65918972b780ecdc55458abf7aab7e5
loenaaaa <asusactiongamer@gmail.com>
2026-01-13 12:49:48 +0800

    change mixin name

--- src/main/java/com/jaquadro/minecraft/storagedrawers/mixins/early/MixinPlayerControllerMP.java
+++ src/main/java/com/jaquadro/minecraft/storagedrawers/mixins/early/MixinPlayerControllerMP.java
@@ -26,7 +26,7 @@ public abstract class MixinPlayerControllerMP {
             at = @At(
                     value = "INVOKE",
                     target = "Lnet/minecraft/block/Block;getPlayerRelativeBlockHardness(Lnet/minecraft/entity/player/EntityPlayer;Lnet/minecraft/world/World;III)F"))
-    private void mixin(int x, int y, int z, int side, CallbackInfo ci) {
+    private void StorageDrawers$checkIfClickingDrawer(int x, int y, int z, int side, CallbackInfo ci) {
         if (mc.theWorld.getTileEntity(x, y, z) instanceof TileEntityDrawers ted) {
             if (ted.getDirection() == side) {
                 final int reach = 5;
