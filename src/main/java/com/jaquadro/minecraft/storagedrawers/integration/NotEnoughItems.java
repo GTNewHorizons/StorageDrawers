47d88409ed534cddad4dea6377cf81295a8187da
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2026-02-21 20:41:35 +0100

    fix error on Server because of NEI integration

--- src/main/java/com/jaquadro/minecraft/storagedrawers/integration/NotEnoughItems.java
+++ src/main/java/com/jaquadro/minecraft/storagedrawers/integration/NotEnoughItems.java
@@ -22,24 +22,12 @@ public final class NotEnoughItems extends IntegrationModule {
     }
 
     @Override
+    protected LoadingSide loadingSide() {
+        return LoadingSide.CLIENT;
+    }
+
+    @Override
     public void init() throws Throwable {
-        // [17:09:38] [Server thread/DEBUG] [LaunchWrapper/StorageDrawers]: Transformer error
-        // java.lang.ClassNotFoundException: Exception caught while transforming class
-        // net.minecraft.client.gui.GuiScreen
-        // at System//net.minecraft.launchwrapper.LaunchClassLoader.findClass(LaunchClassLoader.java:295)
-        // [LaunchClassLoader.class:?]
-        // at java.base/java.lang.ClassLoader.loadClass(ClassLoader.java:557) [?:?]
-        // at java.base/java.lang.ClassLoader.loadClass(ClassLoader.java:490) [?:?]
-        // at java.base/java.lang.Class.forName0(Native Method) [?:?]
-        // at java.base/java.lang.Class.forName(Class.java:467) [?:?]
-        // at java.base/java.lang.Class.forName(Class.java:458) [?:?]
-        // at Launch//com.jaquadro.minecraft.storagedrawers.integration.NotEnoughItems.init(NotEnoughItems.java:23)
-        // [NotEnoughItems.class:?]
-        // at
-        // Launch//com.jaquadro.minecraft.storagedrawers.integration.IntegrationRegistry.init(IntegrationRegistry.java:63)
-        // [IntegrationRegistry.class:?]
-        // at Launch//com.jaquadro.minecraft.storagedrawers.StorageDrawers.init(StorageDrawers.java:108)
-        // [StorageDrawers.class:?]
         Class<?> clGuiContainerManager = Class.forName("codechicken.nei.guihook.GuiContainerManager");
         fdDrawItems = clGuiContainerManager.getDeclaredField("drawItems");
     }
